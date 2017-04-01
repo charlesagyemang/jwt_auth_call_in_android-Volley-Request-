29de20ed7c27368a375688430d6e5cc03648b27c
charles agyemang <micnkru@gmail.com>
2017-04-01 18:00:43 +0000

    added a menu that will help me get a butoon at the top to show the add activity thing where i will set input fields and get my post request up and running

--- app/src/main/java/com/example/pianoafrik/volleytest/CustomVolley.java
+++ app/src/main/java/com/example/pianoafrik/volleytest/CustomVolley.java
@@ -5,10 +5,8 @@ import com.android.volley.ParseError;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.toolbox.HttpHeaderParser;
-
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.UnsupportedEncodingException;
 import java.util.Map;
 
--- app/src/main/java/com/example/pianoafrik/volleytest/MainActivity.java
+++ app/src/main/java/com/example/pianoafrik/volleytest/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.pianoafrik.volleytest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
@@ -123,6 +126,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.add_new:
+                Toast.makeText(this, "add", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
 
